e225c8c795dfc88725cf401ea2c8b2146656868b
Дмитрий Степанов <stepanovworks@mail.ru>
2023-08-01 19:23:43 +0500

    nenavizhu sraniy docker

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,8 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('TOKEN', 'django-insecure-123')
 
-DEBUG = os.getenv('DEBUG', default=False)
-
+DEBUG = os.getenv('DEBUG', False)
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 
 INSTALLED_APPS = [
